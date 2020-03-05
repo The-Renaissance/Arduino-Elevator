026717568f11e4d63c57bfa56efe5bf97f1c2d48
The-Renaissance <nashesvobodnoye@hotmail.com>
2020-03-04 20:21:20 -0500

    Initial commit

---
+++ sketch_mar04a.ino
@@ -0,0 +1,56 @@
+#define FLOORS 4
+static const uint8_t buttons[FLOORS] = {8,9,10,11};
+static const uint8_t leds[FLOORS] = {4,5,6,7};
+
+typedef enum {
+  UP = 0,
+  DOWN,
+  STATIONARY
+} elevator_dir;
+
+static elevator_dir direction;
+
+static volatile bool floor_requested[FLOORS] = {false, false, false, false};
+static volatile int up_requests, down_requests;
+static int current_floor;
+
+void setup() {
+  // Initialize LEDs and buttons
+  for (uint8_t i = 0; i < FLOORS; ++i) {
+    pinMode(leds[i], OUTPUT);
+    pinMode(buttons[i], INPUT_PULLUP);
+  }
+
+  // In the beginning, the elevator is stationary and stays on the first floor
+  digitalWrite(leds[0], HIGH);
+  state = STATIONARY;
+}
+
+void loop() {
+  // change elevator direction based on number of up and down requests
+  if (up_requests == 0 && down_requests != 0) {
+    direction = DOWN;
+  } else if (up_requests != 0 && down_requests == 0) {
+    direction = UP;
+  } else if (up_requests == 0 && down_requests == 0) {
+    direction = STATIONARY;
+  }
+
+  // if no request is received and elevator already on the first floor, do nothing.
+  if (direction == STATIONARY && current_floor == 0) return;
+
+  // based on current floor and direction, determines if the elevator reaches a desired floor.
+  // If yes, add 5s to the delay to simulate people going in and out of the elevator
+  if (floor_requested[current_floor] == true) {
+    floor_requested[current_floor] = false;
+    delay(5000);
+  }
+  delay(1500); // simulated 1.5s delay for traversing one floor
+  if (direction == DOWN) {
+    digitalWrite(leds[current_floor--], LOW);
+    digitalWrite(leds[current_floor], HIGH);
+  } else if (direction == UP) {
+    digitalWrite(leds[current_floor++], LOW);
+    digitalWrite(leds[current_floor], HIGH);
+  }
+}
